cac1891cbf474b90850f2a31194a79a9cda3ac23
ChavanRushikesh <@chavanrushi1246@gmail.com>
2024-03-18 22:53:21 +0530

    update
    new update

---
+++ java.js
@@ -0,0 +1,17 @@
+function showValue1(value) {
+    var mark = document.querySelector('.meter1 .mark');
+    mark.style.width = value + '%';
+  }
+  function showValue2(value) {
+    var mark = document.querySelector('.meter2 .mark');
+    mark.style.width = value + '%';
+  }
+  function showValue3(value) {
+    var mark = document.querySelector('.meter3 .mark');
+    mark.style.width = value + '%';
+  }
+  function showValue4(value) {
+    var mark = document.querySelector('.meter4 .mark');
+    mark.style.width = value + '%';
+  }
+  

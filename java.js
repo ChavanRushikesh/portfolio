63ba31d04bc71cfa725ab6839edf6346da4af528
ChavanRushikesh <@chavanrushi1246@gmail.com>
2024-03-19 13:45:14 +0530

    day 2
    services development

--- java.js
+++ java.js
@@ -1,10 +1,12 @@
 function showValue1(value) {
     var mark = document.querySelector('.meter1 .mark');
     mark.style.width = value + '%';
+    
   }
   function showValue2(value) {
     var mark = document.querySelector('.meter2 .mark');
     mark.style.width = value + '%';
+
   }
   function showValue3(value) {
     var mark = document.querySelector('.meter3 .mark');
@@ -12,6 +14,8 @@ function showValue1(value) {
   }
   function showValue4(value) {
     var mark = document.querySelector('.meter4 .mark');
-    mark.style.width = value + '%';
+    mark.style.width = value + '%'; 
   }
+  
+
   
